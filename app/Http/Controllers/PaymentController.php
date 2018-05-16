88fd92c5df8565bd1d92372c5a525c5d5e668737
arka.yulianto <yulianto.saparudin@arkamaya.co.id>
2018-05-16 09:20:15 +0700

    apalah apalah apalah apalah apalah apalaha apaalajaba

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -25,7 +25,7 @@ class PaymentController extends Controller
 
       $json_result = file_get_contents('php://input');
       $result = stripslashes(trim($json_result, '"'));
-      return \Log::info($result);
+      return \Log::info(json_decode($result));
 
       // $order = new Order;
       // $order->number = $result['order_id'];
